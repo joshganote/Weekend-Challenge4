f7725684afaac0d13caeebf5a4fb4edbc368b4c6
Josh Ganote <j.ganote0825@gmail.com>
2019-12-08 01:30:48 -0600

    attempted conditional render in gallerylistitem.js

--- src/components/GalleryListItem/GalleryListItem.js
+++ src/components/GalleryListItem/GalleryListItem.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 import './GalleryListItem.css';
 
 class GalleryListItem extends Component {
+    // constructor(props) {
+    //     super(props);
+
+    //     this.handleImageDescription = this.handleImageDescription.bind(this);
+    //     this.handleImageNoDescription = this.handleImageNoDescription.bind(this);
+    //     this.state = {showDescription: false};
+    // }
+
+    // handleImageDescription() {
+    //     this.setState({
+    //         showDescription: true
+    //     });
+    // }
+
+    // handleImageNoDescription() {
+    //     this.setState({
+    //         showDescription: false
+    //     });
+    // }
     
     
     render() {
+        // const showDescription = this.state.showDescription;
+
+        // if (showDescription) {
+        //     showClick = 'isLiked';
+        // } else if (!this.state.show) {
+        //     showClick = 'notLiked'
+        // }
 
         return (
             <div>
@@ -18,7 +44,7 @@ class GalleryListItem extends Component {
                 <button className="clickable" onClick={() => this.props.imageLike(this.props.image.id)}>Like</button>
                 <br></br>
                     <ul>
-                        <p>Any hints to get started with like counter plz</p>
+                        <p>Any help with conditional render for img click and likes?</p>
                     </ul>
                 </div>
             </div>
