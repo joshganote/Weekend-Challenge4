8abb10040b9a4b62468245c2348d1bf7e949bbbf
Josh Ganote <j.ganote0825@gmail.com>
2019-12-07 19:05:20 -0600

    added images to app.js, and added them into array in data.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,10 +10,19 @@ class App extends Component {
         </header>
         <br/>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/mid.png" alt="TheMidnightBand"/>
+        <img src="images/squad.png" alt="Friends"/>
+        <img src="images/fam.png" alt="Family"/><br></br>
+        <img src="images/guitar.png" alt="Guitar"/>
+        <img src="images/momz.png" alt="Mom"/>
+        <img src="images/studio.png" alt="Studio"/><br></br>
+        <img src="images/work.png" alt="Work"/>
+        <img src="images/album.png" alt="Album"/>
+        <img src="images/RR.2.png" alt="RR"/>
+
       </div>
     );
   }
 }
 
-export default App;
+export default App; 
